704f9fb27d1a8e5f2338a30a2de83c971869be5a
Young-Mason <mason.yk3@gmail.com>
2021-02-21 13:03:15 +0900

    delete mongoDB info

--- booking.py
+++ booking.py
@@ -1,15 +1,16 @@
+import datetime
 import requests
 from bs4 import BeautifulSoup
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://user:0615@cluster0-cfvlo.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("")
 db = client.booking
 
-import datetime
 t = datetime.datetime.today()
 today = t.strftime('%Y-%m-%d')
 later = t + datetime.timedelta(days=60)
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.booking.com/hotel/kr/96-bunz-travellers-lodge.ko.html?label=gen173nr-1FCAEoggI46AdIM1gEaH2IAQGYARe4ARjIAQzYAQHoAQH4AQuIAgGoAgTYAgE;sid=4c39997eedb9cd5480e633c725b00532;all_sr_blocks=177786902_91037544_0_0_0;checkin=2020-06-23;checkout=2020-06-24;dest_id=-716583;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=1;highlighted_blocks=177786902_91037544_0_0_0;hpos=1;no_rooms=1;room1=A%2CA;sb_price_type=total;sr_order=popularity;sr_pri_blocks=177786902_91037544_0_0_0__5269999;srepoch=1592108378;srpvid=7c051e6d0c0c0067;type=total;ucfs=1&#hotelTmpl', headers=headers)
 
 # today, today+1
@@ -33,11 +34,10 @@ for room in rooms:
         print(type_removed, price_removed)
 
 
-#질문1 : 숫자에서 콤마 없애는법, 파이썬 parse int
+# 질문1 : 숫자에서 콤마 없애는법, 파이썬 parse int
 
 
-
-#질문2 : 오늘부터 향후 60일의 데이터를 크롤링 해야하는 것을 어떻게 코드로 표현해야 할지?
+# 질문2 : 오늘부터 향후 60일의 데이터를 크롤링 해야하는 것을 어떻게 코드로 표현해야 할지?
 #
 # datetime 검색
 # yyyy-mm-dd 포맷
